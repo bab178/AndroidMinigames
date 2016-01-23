18fc8e3d5e789f9036ba42b7669df69fdf4d12ca
Blake Bordovsky <bab178@txstate.edu>
2016-01-22 23:40:44 -0600

    Initial Listview of Minigames

---
+++ AndroidMinigames/app/src/main/java/com/example/blake/androidminigames/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.blake.androidminigames;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] {
+                "Tic-Tac-Toe",
+                "Minesweeper",
+                "Game of Life"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.tile, R.id.tile_text, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) listView.getItemAtPosition(position);
+
+
+
+                switch (itemPosition) {
+                    case 0:
+                        startActivity(new Intent(MainActivity.this, TicTacToe.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(MainActivity.this, Minesweeper.class));
+                        break;
+                    case 2:
+                        startActivity(new Intent(MainActivity.this, GameOfLife.class));
+                        break;
+                }
+
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ AndroidMinigames/app/src/main/java/com/example/blake/androidminigames/Minesweeper.java
@@ -0,0 +1,14 @@
+package com.example.blake.androidminigames;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Minesweeper extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.minesweeper);
+    }
+
+}
